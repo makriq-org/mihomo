c20cc494371a88b03111e4127c4f1e236c77811a
makriq <141617622+makriq@users.noreply.github.com>
2026-09-20 21:11:52 +0300

    Обновить Mihomo до v1.19.31 и сохранить интеграцию FlClash

--- config/config.go
+++ config/config.go
@@ -993,6 +993,7 @@ func parseProxies(cfg *RawConfig) (proxies map[string]C.Proxy, providersMap map[
 		return nil, nil, err
 	}
 
+	SetProxyNameList(proxyList)
 	return proxies, providersMap, nil
 }
 
--- constant/version.go
+++ constant/version.go
@@ -2,7 +2,7 @@ package constant
 
 var (
 	Meta       = true
-	Version    = "1.10.0"
+	Version    = "v1.19.31-flclash"
 	BuildTime  = "unknown time"
 	MihomoName = "mihomo"
 )
